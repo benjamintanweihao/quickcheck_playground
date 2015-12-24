074cbfb3f635cc2908ad918c3ae5d7412b284000
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-12-24 22:53:56 +0800

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule QuickcheckPlayground.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :quickcheck_playground,
+     version: "0.0.1",
+     elixir: "~> 1.2-rc",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:excheck, "~> 0.3", only: :test},
+      {:triq, github: "krestenkrab/triq", only: :test}
+    ]
+  end
+end
